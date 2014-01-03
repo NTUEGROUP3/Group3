122b7c575512401ea7781a4e07b5f44402581ebd
WeiYiChu <emily830125@yahoo.com.tw>
2014-01-03 11:12:43 +0800

    Update post.php

--- post.php
+++ post.php
@@ -4,14 +4,15 @@
   $author = $_POST["author"];
   $subject = $_POST["subject"];
   $content = $_POST["content"];
+  $country = $_POST["country"];
   $current_time = date("Y-m-d H:i:s");
 
   
   $link = create_connection();
 
   
-  $sql = "INSERT INTO message(author, subject, content, date)
-          VALUES('$author', '$subject', '$content', '$current_time')";
+  $sql = "INSERT INTO message(author, subject, content, date, country)
+          VALUES('$author', '$subject', '$content', '$current_time', '$country')";
   $result = execute_sql("Group3", $sql, $link);
 
   
