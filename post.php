512a605eec91fc99cef151cb27d960f017455011
WeiYiChu <emily830125@yahoo.com.tw>
2014-01-05 20:22:06 +0800

    Final

--- post.php
+++ post.php
@@ -1,11 +1,11 @@
 <?php
   require_once("dbtools.inc.php");
 	
-  $author = $_POST["author"];
-  $subject = $_POST["subject"];
-  $content = $_POST["content"];
-  $country = $_POST["country"];
-  $current_time = date("Y-m-d H:i:s");
+  $author = $_POST["author"]; //將使用者輸入的姓名存入變數author中
+  $subject = $_POST["subject"]; //將使用者輸入的主題存入變數subject中
+  $content = $_POST["content"]; //將使用者輸入的內容存入變數content中
+  $country = $_POST["country"]; //將使用者輸入的所在地存入變數country中
+  $current_time = date("Y-m-d H:i:s"); //將使用者留言的時間存入變數current_time中
 
   
   $link = create_connection();
