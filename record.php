7d8edf73064dd85b88f0c8e40d8ea09a26437b1a
WeiYiChu <emily830125@yahoo.com.tw>
2014-01-05 20:32:38 +0800

    Final

--- record.php
+++ record.php
@@ -5,23 +5,23 @@
 include("mysql_connect.php");
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
+<title>國立臺北教育大學_Group3_數學學習網_歷史紀錄</title>
 </head>
 
-<body>
+<body bgcolor="BLACK" text="WHITE" link="#FF00CC" vlink="#FFCC33" alink="FF9900">
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<p align="center"><img src="design/titlefig.jpg"></p>
 </div>
 <div id="MAIN_NAV">
 	<ul>
-		<li><a href="reading.php">開始閱讀</a></li>
-		<li><a href="upload.php">檔案上傳</a></li>
-		<li><a href="message.php">留言板</a></li>
-		<li><b>歷史紀錄</b></li>
-		<li><a href="login.php">會員資料修改</a></li>
-		<li><a href="group.php">管理團隊</a></li>
-		<li><a href="manager_login.php">管理者專區</a></li>
-		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
+		<li><a href="reading.php">開始閱讀</a></li> <!--點選即跳到reading頁面-->
+		<li><a href="upload.php">檔案上傳</a></li> <!--點選即跳到upload頁面-->
+		<li><a href="message.php">留言板</a></li> <!--點選即跳到message頁面-->
+		<li><b>歷史紀錄</b></li> <!--此頁面即為歷史紀錄因此直接印出-->
+		<li><a href="login.php">會員資料修改</a></li> <!--點選即跳到login頁面-->
+		<li><a href="group.php">管理團隊</a></li> <!--點選即跳到group頁面-->
+		<li><a href="manager_login.php">管理者專區</a></li> <!--點選即跳到manager_login頁面-->
+		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li> <!--點選即跳到index頁面-->
 	</ul>
 </div>
 <div id="CONTENT">
@@ -32,12 +32,15 @@ include("mysql_connect.php");
 		<br/>
 		<h3>
 
-		<form name="form" method="post" action=" record_connect.php">
+		<form name="form" method="post" action=" record_connect.php"> <!--帳號及密碼正確即跳到record_connect頁面-->
 			<p>
-			帳號：<input type="text" name="id" /> <br>
-			密碼：<input type="password" name="pw" /> <br>
+			<table border=1 cellspacing="2">
+			<td align=left>帳號：<input type="text" name="id" /> <br></td> <!--讓使用者輸入帳號-->
+			<tr>
+			<td align=left>密碼：</h1><input type="password" name="pw" /> <br></td> <!--讓使用者輸入密碼-->
+			</table> 
 			</p>
-			<input type="submit" name="button" value="送出" />
+			<input type="submit" name="button" value="送出" /> <!--點選即送出所輸帳密-->
 			<p>
 			</p>
 		</form>
@@ -49,7 +52,7 @@ include("mysql_connect.php");
 <div id="FOOTER">	
 	<p>
 		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
+		<h2><center><br/>Author by <i>GROUP THREE</i> </center></h2>
 	</p>
 </div>
 </body>
