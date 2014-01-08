72a50f7a3d9ccea0732022b39205d94700eb4bd2
sara110113045 <jsv2109@yahoo.com.tw>
2014-01-09 00:14:04 +0800

    Final

--- grade6.php
+++ grade6.php
@@ -36,7 +36,7 @@ $row = mysql_fetch_array(mysql_query($sql));
 				echo $id;
 				echo '<img src="design\titlefig6.jpg">';
 				echo "<br/><h2>請選擇閱讀材料<br/></h2><br/><br/>";
-				$sql = "SELECT * FROM grade6";
+				$sql = "SELECT * FROM grade6";//FROM資料庫grade6
 				$result = mysql_query($sql);
 											
 				if (!$result) { 
