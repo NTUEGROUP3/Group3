75fe002631e035ff3a3fc0d753dca5b7650a7851
ning03 <st030228@yahoo.com.tw>
2014-01-10 01:31:03 +0800

    FINAL

--- dbtools.inc.php
+++ dbtools.inc.php
@@ -2,7 +2,8 @@
   function create_connection()
   {
     $link = mysql_connect("localhost", "root", "NTUE")
-      or die("無法建立資料連接<br><br>" . mysql_error());
+      or die("無法建立資料連接<br><br>" . mysql_error());//若未成功建立資料連接會顯示
+     
 	  
     mysql_query("SET NAMES utf8");
 			   	
@@ -12,7 +13,7 @@
   function execute_sql($database, $sql, $link)
   {
     $db_selected = mysql_select_db($database, $link)
-      or die("開啟資料庫失敗<br><br>" . mysql_error($link));
+      or die("開啟資料庫失敗<br><br>" . mysql_error($link));//開啟資料庫連接失敗
 						 
     $result = mysql_query($sql, $link);
 		
