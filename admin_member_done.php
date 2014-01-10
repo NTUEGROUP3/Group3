a57edbb2e10a431a83852f99b7d827413fb02727
ling132012 <ninat6608@gmail.com>
2014-01-10 10:31:02 +0800

    Final

--- admin_member_done.php
+++ admin_member_done.php
@@ -12,57 +12,62 @@
 	$button = $_POST['button'];
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title> <!--這是網頁的標題-->
+<title>國立臺北教育大學_Group3_數學學習網_管理者專區</title>
 </head>
 
-<body>
+<body bgcolor="BLACK" text="WHITE" link="#FF00CC" vlink="#FFCC33" alink="FF9900">
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<p align="center"><img src="design/titlefig.jpg"></p>
 </div>
 <div id="MAIN_NAV">
 	<ul>
-		<li><a href="reading.php">開始閱讀</a></li>  <!--按了後開始閱讀-->
-		<li><a href="upload.php">檔案上傳</a></li>  <!--按了可以上傳檔案-->
-		<li><a href="message.php">留言板</a></li>   <!--按了能到留言板-->
-		<li><a href="record.php">歷史紀錄</a></li>   <!--按了可以看歷史紀錄-->
-		<li><a href="login.php">會員資料修改</li>  <!--按了可以做資料修改-->
-		<li><a href="group.php">管理團隊</a></li>  <!--按了可以去修改團隊-->
-		<li><b>管理者專區</b></li>  <!--按了會到管理者專區-->
-		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li> <!--按了會回首頁-->
+		<li><a href="reading.php">開始閱讀</a></li> 
+		<li><a href="upload.php">檔案上傳</a></li>
+		<li><a href="message.php">留言板</a></li>
+		<li><a href="record.php">歷史紀錄</a></li>
+		<li><a href="login.php">會員資料修改</li>
+		<li><a href="group.php">管理團隊</a></li>
+		<li><b>管理者專區</b></li>
+		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
 	</ul>
 </div>
 <div id="CONTENT">
 	<p>
+		<center>
 		<h2>管理者專區<br/></h2>
 		<br/>
 		<br/>
-		<center>
+		
+		
 		<?php
+		    //若按了新增，會從資料庫新增資料
 			if($button === "新增"){ 
 				$sql = "INSERT INTO member (account, password, username, email, country, age) VALUES ('$account', '$password', '$username', '$email', '$country', '$age')"; //要insert的value值
 				$result = mysql_query($sql);
 				if (!$result) { /
 					die('Invalid query: ' . mysql_error());
 				}
-				echo "<h3>資料已新增.....</h3>"; //顯示已新增的資料
+				echo "<h3>資料已新增.....</h3>";//顯示資料已新增
 			}
+			//若按了修改，會從資料庫修改資料
 			else if($button === "修改"){  
 				$sql = "UPDATE member SET account='$account', password='$password', username='$username', email='$email', country='$country', age='$age' WHERE account='$account'";//要update的value值
 				$result = mysql_query($sql);
 				if (!$result) { 
 					die('Invalid query: ' . mysql_error());
 				}
-				echo "<h3>資料已修改.....</h3>";  //顯示修改的資料
-			}	
-						
+				echo "<h3>資料已修改.....</h3>";//顯示資料已修改
+			}			
 		?>
 		</center>
+		<br/>
+		<br/>
 	</p>	
 </div>
 <div id="FOOTER">	
 	<p>
 		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Kung-Si Cheng</i> </center></h2>
+		<h2><center><br/>Author by <i>GROUP THREE</i> </center></h2>
 	</p>
 </div>
 </body>
