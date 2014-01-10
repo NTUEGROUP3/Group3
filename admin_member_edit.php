fedbb757875fc0c063a4bfec6c1447da361b8341
ling132012 <ninat6608@gmail.com>
2014-01-10 10:31:20 +0800

    Final

--- admin_member_edit.php
+++ admin_member_edit.php
@@ -7,12 +7,12 @@
 	$button = $_POST['button1'];  
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
+<title>國立臺北教育大學_Group3_數學學習網_管理者專區</title>
 </head>
 
-<body>
+<body bgcolor="BLACK" text="WHITE" link="#FF00CC" vlink="#FFCC33" alink="FF9900">
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<p align="center"><img src="design/titlefig.jpg"></p>
 </div>
 <div id="MAIN_NAV">
 	<ul>
@@ -28,29 +28,37 @@
 </div>
 <div id="CONTENT">
 	<p>
-		<h2>管理者專區<br/></h2>
-		<br/>
-		<br/>
 		<center>
+		<h2>使用者資料修改<br/></h2>
+		<br/>
+		<br/>
+		
 		<?php
-			
+			//若按了新增，可新增帳號、密碼、使用者姓名、Email、城市、年齡
 			if($button === "新增"){　
 		?>
 				<form name="form" method="post" action="admin_member_done.php">
 					<p>
-					帳號：<input type="text" name="account" /> <br>
-					密碼：</h1><input type="text" name="pw" /> <br>
-					使用者姓名：</h1><input type="text" name="username" /> <br>
-					Email：</h1><input type="text" name="email" /> <br>
-					城市：</h1><input type="text" name="country" /> <br>
-					年齡：</h1><input type="text" name="age" /> <br>
+					<table border=1 cellspacing="2">
+					<td align=left>帳號：<input type="text" name="account" /> <br></td>
+					<tr>
+					<td align=left>密碼：</h1><input type="text" name="pw" /> <br></td>
+					<tr>
+					<td align=left>使用者姓名：</h1><input type="text" name="username" /> <br></td>
+					<tr>
+					<td align=left>Email：</h1><input type="text" name="email" /> <br></td>
+					<tr>
+					<td align=left>城市：</h1><input type="text" name="country" /> <br></td>
+					<tr>
+					<td align=left>年齡：</h1><input type="text" name="age" /> <br></td>
+					</table>
 					</p>
-					<input type="submit" name="button" value="新增" />
-					<p>
-					</p>
+					<input type="submit" name="button" value="新增" /> <!--若按了新增，剛剛新增的資料會輸入資料庫-->
 				</form>
+				
 		<?php
 			}
+			//若按了修改，可修改帳號、密碼、使用者姓名、Email、城市、年齡
 			else if($button === "修改"){
 				$sql = "SELECT * FROM member WHERE account='$account'";
 				$result = mysql_query($sql);
@@ -63,20 +71,26 @@
 		?>
 					<form name="form" method="post" action="admin_member_done.php"> 
 						<p>
-						帳號：<input type="text" name="account" value="<?php echo $row['account']; ?>" /> <br>
-						密碼：</h1><input type="text" name="pw" value="<?php echo $row['password']; ?>"/> <br>
-						使用者姓名：</h1><input type="text" name="username" value="<?php echo $row['username']; ?>" /> <br>
-						Email：</h1><input type="text" name="email" value="<?php echo $row['email']; ?>" /> <br>
-						城市：</h1><input type="text" name="country" value="<?php echo $row['country']; ?>" /> <br>
-						年齡：</h1><input type="text" name="age" value="<?php echo $row['age']; ?>" /> <br>
+						<table border=1 cellspacing="2">
+						<td align=left>帳號：<input type="text" name="account" value="<?php echo $row['account']; ?>" /> <br></td>
+						<tr>
+						<td align=left>密碼：</h1><input type="text" name="pw" value="<?php echo $row['password']; ?>"/> <br></td>
+						<tr>
+						<td align=left>使用者姓名：</h1><input type="text" name="username" value="<?php echo $row['username']; ?>" /> <br></td>
+						<tr>
+						<td align=left>Email：</h1><input type="text" name="email" value="<?php echo $row['email']; ?>" /> <br></td>
+						<tr>
+						<td align=left>城市：</h1><input type="text" name="country" value="<?php echo $row['country']; ?>" /> <br></td>
+						<tr>
+						<td align=left>年齡：</h1><input type="text" name="age" value="<?php echo $row['age']; ?>" /> <br></td>
+						</table>
 						</p>
-						<input type="submit" name="button" value="修改" />
-						<p>
-						</p>
+						<input type="submit" name="button" value="修改" /> <!--若按了修改，剛剛修改的資料會輸入資料庫-->
 					</form>
 		<?php
 				}
 			}
+			//若按了刪除，可修改帳號、密碼、使用者姓名、Email、城市、年齡
 			else if($button === "刪除"){ 
 				$sql = "DELETE FROM member WHERE account='$account'"; 
 				$result = mysql_query($sql);
@@ -85,17 +99,19 @@
 					die('Invalid query: ' . mysql_error());
 				}
 				else{
-					echo "<h3>資料已刪除.....</h3>";
+					echo "<h3>資料已刪除.....</h3>";//會顯示資料已刪除
 				}
 			}
 		?>
 		</center>
+		<br/>
+		<br/>
 	</p>	
 </div>
 <div id="FOOTER">	
 	<p>
 		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Kung-Si Cheng</i> </center></h2>
+		<h2><center><br/>Author by <i>GROUP THREE</i> </center></h2>
 	</p>
 </div>
 </body>
